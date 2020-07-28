ef8e29473ee867f7824cb5b3d4db6957ede4a697
DanieleM1999 <danielemirauta@gmail.com>
2020-07-28 17:27:35 +0200

    Added some classes.

---
+++ restapplication-spring/src/main/java/it/dstech/configuration/JwtUser.java
@@ -0,0 +1,70 @@
+package it.dstech.configuration;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class JwtUser implements UserDetails {
+
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+    private boolean enabled;
+
+    public JwtUser(
+            String username,
+            String password, Collection<? extends GrantedAuthority> authorities,
+            boolean enabled
+    ) {
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+        this.enabled = enabled;
+    }
+
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+
+    @JsonIgnore
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+}
---
+++ restapplication-spring/src/main/java/it/dstech/configuration/JwtUserFactory.java
@@ -0,0 +1,30 @@
+package it.dstech.configuration;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import it.dstech.model.Authority;
+import it.dstech.model.User;
+import it.dstech.configuration.JwtUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public final class JwtUserFactory {
+
+    private JwtUserFactory() {
+    }
+
+    public static JwtUser create(User user) {
+        return new JwtUser(
+                user.getUsername(),
+                user.getPassword(),
+                mapToGrantedAuthorities(user.getAuthorities()),
+                user.getEnabled()
+        );
+    }
+
+    private static List<GrantedAuthority> mapToGrantedAuthorities(List<Authority> authorities) {
+        return authorities.stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getName().name()))
+                .collect(Collectors.toList());
+    }
+}
